30c9ce429d4a5d47a632d536a91154355b84d0b0
mohamed-frah <mohamed.frah@laplateformeio>
2023-11-20 15:46:43 +0100

    pour aller plus loin

--- jour01/job09/main.py
+++ jour01/job09/main.py
@@ -2,7 +2,7 @@ nom = "orange"
 prix_unitaire  = 100
 quantité_en_stock = 150
 print(nom , prix_unitaire , quantité_en_stock)
-nouveau_stock = quantité_en_stock + int(input("le nouveau stock d'orange" ))
+nouveau_stock = quantité_en_stock + int(input("combien voulez-vous d'orange : " ))
 print(nouveau_stock)
 nouveau_prix = (prix_unitaire * 1.1 )
 print(nouveau_prix)
---
+++ jour01/pour aller plus loin/main.py
@@ -0,0 +1,5 @@
+abc = input("entrer un mot : ")
+if "e" in abc : 
+    print ( "Il y a un e dans mon paragraphe" )
+else : 
+    print ("il n'y a pas de e dans mon paragraphe")
