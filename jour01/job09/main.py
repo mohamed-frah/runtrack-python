f3c74d456f9310f959e03de7d498ec43cd4e4b4f
mohamed-frah <mohamed.frah@laplateformeio>
2023-11-20 15:23:38 +0100

    la fin

---
+++ jour01/job09/main.py
@@ -0,0 +1,8 @@
+nom = "orange"
+prix_unitaire  = 100
+quantité_en_stock = 150
+print(nom , prix_unitaire , quantité_en_stock)
+nouveau_stock = quantité_en_stock + int(input("le nouveau stock d'orange" ))
+print(nouveau_stock)
+nouveau_prix = (prix_unitaire * 1.1 )
+print(nouveau_prix)
---
+++ jour01/job10/main.py
@@ -0,0 +1,12 @@
+montant = 10000
+taux = 1.02
+montant = montant * taux
+print(montant)
+montant = montant + 5000
+taux = taux + 0.02
+montant = montant * taux
+print(montant)
+montant = montant * 0.9
+taux = taux - 1
+print(montant)
+
