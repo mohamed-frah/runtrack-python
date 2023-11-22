f543681cde3de759bc84c70164de29401ed29cb4
mohamed-frah <146708383+mohamed-frah@users.noreply.github.com>
2023-11-22 16:54:28 +0100

    Update main.py

--- jour03/job10/main.py
+++ jour03/job10/main.py
@@ -1,8 +1,10 @@
 def pair_impair(chiffre):
-    if chiffre % 2 == 0:
-        print("pair")
-    elif chiffre % 2 != 0:
-        print("impair")
-    chiffre  >= 0
+    if chiffre >0 and type(chiffre) == int:
+        if chiffre % 2 == 0:
+            print("pair")
+        elif chiffre % 2 != 0:
+            print("impair")
+    else:
+        print("votre chiffre ne serre a rien")
 
-pair_impair(55)
+pair_impair(-1001)
