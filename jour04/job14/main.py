0ca821d1bc569c8c600795654bea69a03ba2a8b1
mohamed-frah <146708383+mohamed-frah@users.noreply.github.com>
2023-11-26 13:36:06 +0100

    Add files via upload

--- jour04/job14/main.py
+++ jour04/job14/main.py
@@ -1,7 +1,5 @@
 def my_long_word(longueur, phrase):
-    """
-    Retourne les mots d'une phrase qui sont plus longs qu'une longueur donnée.
-    """
+   
     words = phrase.split()  
     long_words = []  
 
@@ -12,13 +10,11 @@ def my_long_word(longueur, phrase):
     return ' '.join(long_words)  
 
 def count_characters(word):
-    """
-    Compte le nombre de caractères dans un mot sans utiliser la fonction len().
-    """
+  
     count = 0
     for caractere in word:
         count += 1
     return count
 
-test_sentence = "Les poubelles des uns font les trésors des autres"
+test_sentence = "La peur est le chemin vers le côté obscur, la peur mène à la colère, la colère mène à la haine, la haine mène à la souffrance"
 print(f"{my_long_word(3, test_sentence)}")
